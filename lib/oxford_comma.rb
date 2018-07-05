bcaf6798a9f7f702726352389f54a5ef338aafe2
casey van-roose <kcvanroose@gmail.com>
2018-07-05 18:36:33 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.join
+  elsif
+    array.length == 2
+    array.join " and "
+  elsif
+    array.length >= 3
+    last_word = "and #{array.pop}"
+    array.push(last_word)
+  #array.insert(-2, "and") 
+    array.join ", "
+  end
 end
